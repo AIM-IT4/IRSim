8fef1a725c4876f9d528b5c4653a814508a5c0bd
Amit Kumar Jha <77675138+AIM-IT4@users.noreply.github.com>
2023-09-24 18:46:30 +0530

    Update __init__.py

--- IRSim/__init__.py
+++ IRSim/__init__.py
@@ -1,8 +1,8 @@
-from .vasicek import vasicek
-from .cir import cir
-from .hull_white_one_factor import hullwhiteonefactor
-from .hull_white_two_factor import hullwhitetwofactor
-from .hjm import hjm
-from .lmm import lmm
-from .bdt import bdt
-from .g2pp import g2pp
+from .vasicek import simulate_vasicek as vasicek
+from .cir import simulate_cir as cir
+from .hull_white_one_factor import simulate_hull_white_one_factor as hullwhiteonefactor
+from .hull_white_two_factor import simulate_hull_white_two_factor as hullwhitetwofactor
+from .hjm import simulate_hjm as hjm
+from .lmm import simulate_lmm as lmm
+from .bdt import simulate_bdt as bdt
+from .g2pp import simulate_g2pp as g2pp
